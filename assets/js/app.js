e8b2629564a0df554711ddeb1e4b45ed7b5c1e95
Chetan Meshram <chmeshra@visa.com>
2017-05-31 23:50:38 -0700

    AJAX call changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,22 @@
 // array for your serach topics
 let topics = [
-    "Husky"
+    "Husky",
+    "Cats"
 ];
 
 var gifButtons = $("#topics-gallery");
 var gifsGallery = $("#gifs-gallery");
 var a = $("<button>");
 
+function addTopic(e) {
+    e.preventDefault();
+    var topic = $("#addTopic").val();
+    topics.push(topic);
+    showTopics(topics);
+}
+
 function displayGifInfo() {
+    gifsGallery.empty();
     var gif = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -18,17 +27,21 @@ function displayGifInfo() {
     }).done(function(response) {
         response.data.map((i) => {
             var li = $("<li>");
+            var h3 = $("<h3>");
             var img = $("<img>");
             var rating = i.rating;
         	var defaultAnimatedSrc = i.images.fixed_height.url;
         	var staticSrc = i.images.fixed_height_still.url;
-            li.addClass("imgGif-container");
+            li.addClass("card imgGif-container");
             img.attr("src", staticSrc);
             img.addClass("imgGif");
             img.attr("data-state", "still");
         	img.attr("data-still", staticSrc);
         	img.attr("data-animate", defaultAnimatedSrc);
-            li.append(img);
+            h3.addClass("card-text");
+            h3.text("Rating: " + rating);
+            li.append(h3);
+            li.prepend(img);
             gifsGallery.append(li);
         });
     });
@@ -45,25 +58,39 @@ function playGif() {
   }
 }
 
-topics.map((i) => {
+function showTopics(topics) {
+    // Deletes the gifs prior to adding new gifs
+    // (this is necessary otherwise you will have repeat buttons)
+    gifButtons.empty();
+    topics.map((i) => {
     // Then dynamicaly generates buttons for each movie in the array
           // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          //var a = $("<button>");
+          var btn = $("<button>");
+          var li = $("<li>");
           // Adds a class of movie to our button
-          a.addClass("gifButton");
+          btn.addClass("gifButton");
           // Added a data-attribute
-          a.attr("data-name", i);
+          btn.attr("data-name", i);
           // Provided the initial button text
-          a.text(i);
+          btn.text(i);
+          li.append(btn);
           // Added the button to the buttons-view div
-          gifButtons.append(a);
-});
+          gifButtons.append(li);
+    });
+};
+
+showTopics(topics);
+
+// Adding click event listeners to add a Topic
+$(document).on("click", "#showTopic", addTopic);
 
 // Adding click event listeners to all elements with a class of "gifButton"
 $(document).on("click", ".gifButton", displayGifInfo);
 
 // Adding click event listeners to all elements with a class of "imgGif-container"
 $(document).on("click", ".imgGif", playGif);
+
+
 
 
 
